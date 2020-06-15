5f93b4221934d3b757586b5837aa9c02edc0df20
pabello <waclawiak.pawel@wp.pl>
2020-06-15 19:19:34 +0200

    Added varying word size

--- public/index.js
+++ public/index.js
@@ -1,6 +1,8 @@
-const width = 800;
-const squareSize = 500;
-const radius = width/2;
+// import d3;
+
+let vpWidth = window.innerWidth * 1.1;
+let vpHeight = window.innerHeight * 1.2;
+const radius = window.innerHeight/2;
 
 var colorin = "#00f"
 var colorout = "#f00"
@@ -10,7 +12,7 @@ var colornone = "#ccc"
 // const svg = d3.select('svg');
 //
 // można bez problemu ustawić ciemne tło
-// svg.style('background-color', 'black')/[]
+// svg.style('background-color', '#ddd')/[]
 
 topic = 'school'
 
@@ -20,7 +22,7 @@ line = d3.lineRadial()
     .angle(d => d.x)
 
 tree = d3.cluster()
-    .size([2 * Math.PI, radius - 100])
+    .size([2 * Math.PI, radius*.95])
 
 leaves = readAnalysis(topic)
    .then(hierarchy)
@@ -34,32 +36,34 @@ leavesMapped = leaves
    .then(flatMap)
    // .then(console.log)
 
+// appearances = root
+//    .then(r => r.)
+
 async function draw() {
 
-   // const root = tree(links(d3.hierarchy(data)
-   //    .sort((a, b) => d3.ascending(a.height, b.height) || d3.ascending(a.data.word, b.data.word))));
-
    const svg = d3.select("svg")
-      .attr("viewBox", [-width / 2, -width / 2, width, width])
-
+      .attr("viewBox", [-vpWidth / 2,
+                        -vpHeight / 2,
+                         vpWidth,
+                         vpHeight])
 
    const node = svg.append("g")
       .attr("font-family", "sans-serif")
-      .attr("font-size", '14px')
       .selectAll("g")
       .data(await leaves)
       .join("g")
       .attr("transform", d => `rotate(${d.x * 180 / Math.PI - 90}) translate(${d.y},0)`)
       .append("text")
-      .attr("dy", "0.31em")
+      .attr("dy", "0.31em")  // center words on nodes
+      .attr("font-size", d => `${5 + d.data.appearances}px`)
       .attr("x", d => d.x < Math.PI ? 6 : -6)
       .attr("text-anchor", d => d.x < Math.PI ? "start" : "end")
       .attr("transform", d => d.x >= Math.PI ? "rotate(180)" : null)
       .text(d => d.data.word.charAt(0).toUpperCase() + d.data.word.slice(1) )
-      .each( (d) => d.text = this )
+      .each( function(d) {d.text = this} )
       .on("mouseover", overed)
       .on("mouseout", outed)
-      .call(text => text.append("title").text(d => `${id(d)}
+      .call(text => text.append("title").text(d => `${d.data.word}
          ${d.connections.length} connections`));
 
    const link = svg.append("g")
@@ -76,14 +80,14 @@ async function draw() {
       link.style("mix-blend-mode", null);
       d3.select(this).attr("font-weight", "bold");
       d3.selectAll(d.connections.map(d => d.path)).attr("stroke", colorout).raise();
-      d3.selectAll(d.connections.map(([, d]) => d.text)).attr("fill", colorout).attr("font-weight", "bold");
+      d3.selectAll(d.connections.map(d => d[1].text)).attr("fill", colorout).attr("font-weight", "bold")
    }
 
    function outed(d) {
       link.style("mix-blend-mode", "multiply");
       d3.select(this).attr("font-weight", null);
       d3.selectAll(d.connections.map(d => d.path)).attr("stroke", null);
-      d3.selectAll(d.connections.map(([, d]) => d.text)).attr("fill", null).attr("font-weight", null);
+      d3.selectAll(d.connections.map(d => d[1].text)).attr("fill", null).attr("font-weight", null);
    }
 
   return svg.node();
